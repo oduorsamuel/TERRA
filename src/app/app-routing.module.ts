5119017d10017c00342e844dffee1c03ced859ff
oduorsamuel <samuel@techfactory.ltd>
2020-05-30 13:27:38 +0300

    added login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'overview-page',
     loadChildren: () => import('./overview-page/overview.module').then(m => m.OverviewModule),
   },
-  { path: '',
-    redirectTo: '/overview-page',
-    pathMatch: 'full'
+  { path: 'login',
+    component : LoginComponent
   },
+  { path: '',
+  redirectTo: 'login',
+  pathMatch: 'full'
+},
 ];
 
 @NgModule({
